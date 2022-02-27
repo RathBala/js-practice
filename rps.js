4c7eac99d1c6e856e16ba6e611ca3acc966aeee7
RathBala <rath.balas1@gmail.com>
2022-02-27 09:15:12 +0000

    made lots of UI changes

--- rps.js
+++ rps.js
@@ -1,11 +1,13 @@
 
+const results = document.querySelector('#results');
+
+// start keeping tabs on player's and computer's scores
+let computerScore = 0;
+let playerScore = 0;
+
+game();
+
 function computerPlay () {
-    // create a string
-    // select a random number between 1 - 3
-    // if 1, set string to Rock
-    // else if 2, set string to Paper
-    // else if 3, set string to Scissors
-    // output string
 
     let computerChoice = "";
     let randomNum = Math.floor(Math.random()*3)+1;
@@ -13,102 +15,147 @@ function computerPlay () {
     switch (randomNum) {
         case 1:
             computerChoice = "rock";
-            console.log("Computer chooses " + computerChoice);
             return computerChoice;
         case 2:
             computerChoice = "paper";
-            console.log("Computer chooses " + computerChoice);
             return computerChoice;
         default:
             computerChoice = "scissors";
-            console.log("Computer chooses " + computerChoice);
             return computerChoice;
     }   
 }
 
 function game () {
+
     // ask player to pick an option each round
 
-    let playerSelection = "";
-    playerSelection = window.prompt("Rock, paper or scissors?");
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-    
+    const btn = document.querySelectorAll('button');
+
+    btn.forEach ((button) => {
+        button.addEventListener('click', () => {
+            console.log(playRound(button.id,computerPlay()));
+        });
+    });
+
     // a function to take player's selection and compare it to computer's selection
 
     function playRound (playerSelection, computerSelection) {
-        // normalise player selection to small caps
-        let normalisedPlayer = playerSelection.toLowerCase();
-        console.log("Player chooses " + normalisedPlayer);
-        let outcome = "";
-        // compare player selection to computer selection
-        // if player picked rock & computer picked scissors = player wins
-        // if player picked paper & computer picked scissors = computer wins
-        // if player picked scissors & computer picked scissors = draw
+      
+        const playerText = document.createElement('p');
+        playerText.textContent = "Player chooses " + playerSelection;
+        results.appendChild(playerText);
+
+        const compText = document.createElement('p');
+        const winStatus = document.createElement('p');
+        const score = document.createElement('p');
 
         if (computerSelection === "scissors") {
 
-            switch (normalisedPlayer) {
+        compText.textContent = "Computer chooses " + computerSelection ;
+        results.appendChild(compText);
+
+            switch (playerSelection) {
                 case "rock":
-                    outcome = "Player wins! :)";
                     playerScore++;
-                    return outcome;
+                    winStatus.textContent = "Player wins! :)";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 case "paper":
-                    outcome = "Computer wins! :(";
                     computerScore++;
-                    return outcome;
+                    winStatus.textContent = "Computer wins! :(";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 case "scissors":
-                    outcome = "Draw! Go again.";
-                    return outcome;
+                    winStatus.textContent = "Draw! Go again.";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 default:
-                    outcome = "What is that?? Go again.";
-                    return outcome;
+                    winStatus.textContent = "What is that?? Go again.";
+                    results.appendChild(winStatus);
+                    break;
             }
         }
 
-        // if player picked rock & computer picked rock = draw
-        // if player picked paper & computer picked rock = player wins
-        // if player picked scissors & computer picked rock = computer wins
 
         else if (computerSelection === "rock") {
-            switch (normalisedPlayer) {
+
+        compText.textContent = "Computer chooses " + computerSelection;
+        results.appendChild(compText);  
+
+            switch (playerSelection) {
                 case "rock":
-                    outcome = "Draw! Go again.";
-                    return outcome;
+                    winStatus.textContent = "Draw! Go again.";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 case "paper":
-                    outcome = "Player wins! :)";
                     playerScore++;
-                    return outcome;
+                    winStatus.textContent = "Player wins! :)";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 case "scissors":
-                    outcome = "Computer wins! :(";
                     computerScore++;
-                    return outcome;
+                    winStatus.textContent = "Computer wins! :(";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 default:
-                    outcome = "What is that?? Go again.";
-                    return outcome;
+                    winStatus.textContent = "What is that?? Go again.";
+                    results.appendChild(winStatus);
+                    break;
             }    
         }
 
-        // if player picked rock & computer picked paper = computer wins
-        // if player picked paper & computer picked paper = draw
-        // if player picked scissors & computer picked paper = player wins
 
         else if (computerSelection === "paper") {
-            switch (normalisedPlayer) {
+
+        compText.textContent = "Computer chooses " + computerSelection;
+        results.appendChild(compText);  
+
+            switch (playerSelection) {
                 case "rock":
-                    outcome = "Computer wins! :(";
                     computerScore++;
-                    return outcome;
+                    winStatus.textContent = "Computer wins! :(";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 case "paper":
-                    outcome = "Draw! Go again.";
-                    return outcome;
+                    winStatus.textContent = "Draw! Go again.";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 case "scissors":
-                    outcome = "Player wins! :)";
                     playerScore++;
-                    return outcome;
+                    winStatus.textContent = "Player wins! :)";
+                    score.textContent = "Computer: " + computerScore + " Player: " + playerScore;
+                    results.appendChild(winStatus);
+                    results.appendChild(score);
+                    scoreCheck();
+                    break;
                 default:
-                    outcome = "What is that?? Go again.";
-                    return outcome;
+                    winStatus.textContent = "What is that?? Go again.";
+                    results.appendChild(winStatus);
+                    break;
             }    
         }
     }
@@ -116,36 +163,22 @@ function game () {
 
 // calculate final score & outcome
 
-function finalScore() {
-    
-    let result = "";
-    console.log("Computer: " + computerScore + " Player: " + playerScore);
+function scoreCheck() {
 
-    if (computerScore > playerScore) {
-        result = "Computer wins the game!";
-    } else if (playerScore > computerScore) {
-        result = "Player wins the game!";
-    } else {
-        result = "Oh wow, it's a draw!";
-    }
+        // check if someone has won the game
 
-    console.log(result);
+        const endResult = document.createElement('h2');
+
+        if (playerScore >= 5) {
+            console.log("player wins");
+            endResult.textContent = "PLAYER WINS THE GAME!";
+            results.appendChild(endResult);
+            return;
+        } else if (computerScore >= 5) {
+            console.log("computer wins");
+            endResult.textContent = "GAME OVER - computer has won the game";
+            results.appendChild(endResult);
+            return;
+        }
+
 }
-
-// start keeping tabs on player's and computer's scores
-let computerScore = 0;
-let playerScore = 0;
-
-// play 5 rounds
-
-for (i = 1; i < 6; i++) {
-    console.log("Computer: " + computerScore + " Player: " + playerScore);
-    console.log ("ROUND " + i);
-    game();
-}
-
-finalScore();
-
-// BUGS TO FIX
-// 1. not displaying 5th round score
-// 2. need to show who ultimately wins the game, not just each round
